070d302d1d3b251720614a934f9a2871d868eb68
Huang Grace <huang.yahui.grace@gmail.com>
2019-12-07 14:14:26 +0800

    陰影

--- HelloNetwork02/ViewController.swift
+++ HelloNetwork02/ViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var containerView: UIView!
     @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        containerView.clipsToBounds = false
+        containerView.backgroundColor = UIColor.clear
+        containerView.layer.shadowRadius = 10         //陰影
+        containerView.layer.shadowOpacity = 0.6;
+        containerView.layer.shadowColor = UIColor.gray.cgColor
+        containerView.layer.shadowOffset = CGSize(width: 10, height: 10)
+        
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = imageView.frame.width / 2         //圓角
+        imageView.layer.borderColor = UIColor.red.cgColor //邊線色
+        imageView.layer.borderWidth = 5          //邊線
+        
+        //imageView.contentMode = UIViewContentModeScaleToFill
+        
         let urlString = "https://image.freepik.com/free-vector/kids-wearing-colorful-costumes-different-superheroes-retro-set-isolated_1284-15219.jpg"
         let start = Date().timeIntervalSince1970
         if let url = URL(string: urlString) {
